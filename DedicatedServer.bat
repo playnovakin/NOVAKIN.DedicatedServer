f920835958c8799008eccb832a6ab331c93b2fd7
playnovakin <35315275+playnovakin@users.noreply.github.com>
2018-06-18 22:54:22 -0700

    Add files via upload

--- DedicatedServer.bat
+++ DedicatedServer.bat
@@ -1,2 +1,2 @@
 "steamcmd\steamcmd.exe" +login anonymous +force_install_dir ../ +app_update 463280 +quit
-NOVAKIN.exe -batchmode -nographics -server -serverconfig serverConfig.json -modname Elite
+NOVAKIN.exe -batchmode -nographics -server serverConfig.json -modname Elite
